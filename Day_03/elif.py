46cc12907be51ab713d941e07045ca5410bc44ce
Khushi Kumari <thenameiskhushi07@gmail.com>
2025-06-03 05:35:10 +0530

    First commit

---
+++ Day_03/elif.py
@@ -0,0 +1,18 @@
+# applePrice = 10
+# budget = 200
+
+# if (budget - applePrice > 50):
+#     print("Okay, add 1 kg Apples to the cart.")
+# elif (budget - applePrice > 70):
+#     print("Its okay you can buy")
+# else:
+#     print("Do not add Apples to the cart.")
+
+num = int(input("Enter a number: "))
+
+if (num < 0):
+    print("Number is negative.")
+elif (num == 0):
+    print("Number is zero.")
+else:
+    print("Number is positive.")
